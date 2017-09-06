67e86cf3153e0e225913cb2b4703e1065ec540ac
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-09-06 20:53:15 +0000

    defined function

---
+++ objects-advanced-practice.js
@@ -0,0 +1,77 @@
+var salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+
+// implement a function that calculates the total sales and total tax, grouped by company.
+
+function calculateSalesTax(salesData, taxRates) {
+  // Implement your code here
+  let results = {}
+
+  for (i = 0; i < salesData.length; i ++) {
+    var salesTotal = 0
+    for (var x in salesData[i].sales) {
+      salesTotal = salesTotal + salesData[i].sales[x]
+    }
+    //var currentProvince = salesData[i].province
+    //var currentTaxRate = taxRates[currentProvince]
+    //var taxTotal = salesTotal * currentTaxRate
+    var taxTotal = salesTotal * taxRates[salesData[i].province]
+
+    if (results[salesData[i].name]) {
+      var oldTotalSales = results[salesData[i].name].totalSales
+      var oldTotalTaxes = results[salesData[i].name].totalTaxes
+      results[salesData[i].name] = {
+        totalSales: oldTotalSales + salesTotal,
+        totalTaxes: oldTotalTaxes + taxTotal
+      }
+    } else {
+      results[salesData[i].name] = {
+        totalSales: salesTotal,
+        totalTaxes: taxTotal
+      }
+    }
+  }
+return results
+}
+
+console.log(calculateSalesTax(companySalesData, salesTaxRates))
+
+
+
+
+
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
+
